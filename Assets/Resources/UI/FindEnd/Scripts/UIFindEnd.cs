19b96ae3f26c37bbd765866bd0be2345bc64abea
Anthony <xuanyylhky@163.com>
2018-02-21 20:24:46 +0800

    Update FindEnd and Main Logic
    补充两个界面的逻辑和动画效果

--- Assets/Resources/UI/FindEnd/Scripts/UIFindEnd.cs
+++ Assets/Resources/UI/FindEnd/Scripts/UIFindEnd.cs
@@ -7,15 +7,22 @@ public class UIFindEnd : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         // 测试， 直接开始游戏
-        Invoke("StartGame", 3);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+        //Invoke("StartGame", 3);
+        UIPlayTween bg = transform.Find("BG").GetComponent<UIPlayTween>();
+
+        Transform blue = transform.Find("Blue");
+        if (blue != null)
+        {
+            EventDelegate.Add(blue.GetComponent<TweenPosition>().onFinished,() => bg.Play(true));
+        }
 	}
 
-    void StartGame()
+    public void StartGame()
+    {
+        Invoke("GOMain", 1);
+    }
+
+    private void GOMain()
     {
         SceneManager.LoadScene("Main");
     }
--- Assets/Resources/UI/Main/Scripts/UIMain.cs
+++ Assets/Resources/UI/Main/Scripts/UIMain.cs
@@ -20,15 +20,18 @@ public class UIMain : MonoBehaviour
     private Transform mRedSlider;
 
     // 问题和回答
+    private Transform mRoundText;
     private Transform mQuestion;
     private List<Transform> mAllAnswer;
 
     // 当局的 AI
     private RedAI mAI;
 
-    // 双方操作
+    // 双方操作 - 玩家和AI是否完成答题
     private bool mPlayerFinish;
+    private Transform mPlayerSelect;
     private bool mAIFinish;
+    private Transform mAISelect;
 
     private float mPlayerScore;
     private float mAIScore;
@@ -44,7 +47,9 @@ public class UIMain : MonoBehaviour
     {
         Init();
         InitEventManager();
-        OneStart();
+
+        // 回合开始， 第一回合
+        StartCoroutine(OneStart());
     }
 
     private void Init()
@@ -56,6 +61,7 @@ public class UIMain : MonoBehaviour
         mBlueSlider = mBody.Find("Blue");
         mRedSlider = mBody.Find("Red");
 
+        mRoundText = mBody.Find("Round");
         mQuestion = mBody.Find("Question");
         mAllAnswer = new List<Transform>();
         for (int i = 0; i < 4; i++)
@@ -105,19 +111,50 @@ public class UIMain : MonoBehaviour
     }
 
     // 一回合开始
-    private void OneStart()
+    IEnumerator OneStart()
     {
-        // 通知UI回合开始，
+        // 先显示第几题 -- 动画播完才出题
+        UIHelper.SetLabel(mRoundText, string.Format("第{0}题", mRound));
+        mRoundText.GetComponent<TweenScale>().PlayForward();
+        yield return new WaitForSeconds(1.3f);
+        mRoundText.GetComponent<TweenScale>().PlayReverse();
+        yield return new WaitForSeconds(0.3f);
+
+        // 更新题目文本
+        Question tmpQ = curQuestions[mRound - 1];
+        UIHelper.SetLabel(mQuestion, tmpQ.text);
+        UIHelper.SetLabel(mAllAnswer[0], "Label", tmpQ.A);
+        UIHelper.SetLabel(mAllAnswer[1], "Label", tmpQ.B);
+        UIHelper.SetLabel(mAllAnswer[2], "Label", tmpQ.C);
+        UIHelper.SetLabel(mAllAnswer[3], "Label", tmpQ.D);
+
+        // 通知AI回合开始，
         BaseEvent data = new BaseEvent();
         data.pEventID = (int)PlayerEvent.BeginReply;
         manager.NotifyEvent(data.pEventID, data);
 
+        // 第一回合的处理
+        if (mRound == 1)
+        {
+            mBody.GetComponent<UIPlayTween>().tweenGroup = 3;
+            mBody.GetComponent<UIPlayTween>().Play(true);
+        }
+
+        // 题目先出来
+        mBody.GetComponent<UIPlayTween>().tweenGroup = 1;
         mBody.GetComponent<UIPlayTween>().Play(true);
 
-        // 所有牌变白
+        // 1s 后再出选项
+        yield return new WaitForSeconds(1f);
+        mBody.GetComponent<UIPlayTween>().tweenGroup = 2;
+        mBody.GetComponent<UIPlayTween>().Play(true);
+
+        // 所有牌变白 - 圆圈隐藏
         for (int i = 0; i < mAllAnswer.Count; i++)
         {
             UIHelper.SetColor(mAllAnswer[i], Color.white);
+            UIHelper.SetActive(mAllAnswer[i], "Right", false);
+            UIHelper.SetActive(mAllAnswer[i], "Left", false);
         }
         // 所有牌能点
         AnswerEnable(true);
@@ -125,21 +162,13 @@ public class UIMain : MonoBehaviour
         // 计时从0开始
         mTimingCoroutine = StartCoroutine(Timing());
 
-        // 更新题目
-        Question tmpQ = curQuestions[mRound-1];
-        UIHelper.SetLabel(mQuestion, tmpQ.text);
-        UIHelper.SetLabel(mAllAnswer[0], "Label", tmpQ.A);
-        UIHelper.SetLabel(mAllAnswer[1], "Label", tmpQ.B);
-        UIHelper.SetLabel(mAllAnswer[2], "Label", tmpQ.C);
-        UIHelper.SetLabel(mAllAnswer[3], "Label", tmpQ.D);
-
         mPlayerFinish = false;
         mAIFinish = false;
     }
 
     IEnumerator Timing()
     {
-        time = 10;
+        time = 9.9f;
         UISprite timeSprite = mTimeCircle.GetComponent<UISprite>();
         while (time >= 0)
         {
@@ -170,16 +199,16 @@ public class UIMain : MonoBehaviour
         AnswerEnable(false);
 
         string click = go.name;
-        
+        mPlayerSelect = go.transform; // 记录玩家的选择
+        // 显示左边的圈
+        UIHelper.SetActive(go.transform, "Left", true);
+
         if (mRound == 1) UIHelper.SetActive(mBlueSlider, "Score", true);
         // 判断正确与否
-
         if(curQuestions[mRound-1].real == click)
         {
             // 正确 判断加多少分
             mPlayerScore += 240 * time / 10f;
-            UIHelper.SetLabel(mBlueSlider, "Score", mPlayerScore.ToString("0"));
-
             UIHelper.SetColor(go.transform, Color.green);
         }
         else
@@ -187,8 +216,24 @@ public class UIMain : MonoBehaviour
             // 错误 不加分
             UIHelper.SetColor(go.transform, Color.red);
         }
-
+        UIHelper.SetLabel(mBlueSlider, "Score", mPlayerScore.ToString("0"));
         UIHelper.SetSlider(mBlueSlider, mPlayerScore / MaxScore);
+
+        if (mAIFinish)
+        {
+            // AI先答， 现在再出AI的答案
+            UIHelper.SetActive(mAISelect, "Right", true);
+            if (click == mAISelect.name)
+            {
+                //AI 对了
+                UIHelper.SetColor(mAISelect, Color.green);
+            }
+            else
+            {
+                UIHelper.SetColor(mAISelect, Color.red);
+            }
+        }
+
         // 判断游戏是否结束
         OneEnd();
     }
@@ -203,22 +248,34 @@ public class UIMain : MonoBehaviour
 
         ExData<int> data = varData as ExData<int>;
         int answer = data.data;
+        mAISelect = mAllAnswer[answer]; // 记录AI的选择
         
+        if (mPlayerFinish) // 玩家先答题， 显示右边的圈
+            UIHelper.SetActive(mAllAnswer[answer], "Right", true);
+
         mAIFinish = true;
         if (mRound == 1) UIHelper.SetActive(mRedSlider, "Score", true);
         // 判断正确与否
-
         if(curQuestions[mRound-1].real == ((char)(65+answer)).ToString())
         {
             // 正确 判断加多少分
             mAIScore += 240 * time / 10f;
             UIHelper.SetSlider(mRedSlider, mAIScore / MaxScore);
-            UIHelper.SetColor(mAllAnswer[answer], Color.green);
+            if (mPlayerFinish) // 玩家先答题才出答案
+                UIHelper.SetColor(mAllAnswer[answer], Color.green);
         }
         else
         {
             // 错误不加分
-            UIHelper.SetColor(mAllAnswer[answer], Color.red);
+            if (mPlayerFinish) // 玩家先答题才出答案
+                UIHelper.SetColor(mAllAnswer[answer], Color.red);
+            else
+            {
+                // 玩家没答题， AI的进度条闪一下
+                TweenColor tc = mRedSlider.Find("Sprite").GetComponent<TweenColor>();
+                tc.ResetToBeginning();
+                tc.PlayForward();
+            }
         }
 
         UIHelper.SetLabel(mRedSlider, "Score", mAIScore.ToString("0"));
@@ -235,6 +292,10 @@ public class UIMain : MonoBehaviour
             return;
         }
 
+        // 显示正确答案
+        Transform realA = mAllAnswer.Find((go)=> curQuestions[mRound - 1].real==go.name);
+        UIHelper.SetColor(realA, Color.green);
+
         // 停止计时
         if (mTimingCoroutine != null)
             StopCoroutine(mTimingCoroutine);
@@ -245,13 +306,17 @@ public class UIMain : MonoBehaviour
     {
         // 等一秒答案消失
         yield return new WaitForSeconds(1);
+        mBody.GetComponent<UIPlayTween>().tweenGroup = 1;
         mBody.GetComponent<UIPlayTween>().Play(false);
-        // 等0.5秒结算
-        yield return new WaitForSeconds(0.5f);
+        mBody.GetComponent<UIPlayTween>().tweenGroup = 2;
+        mBody.GetComponent<UIPlayTween>().Play(false);
+        // 等一会结算
+        yield return new WaitForSeconds(1f);
         if (mRound < 5)
         {
             mRound++;
-            OneStart();
+            // 回合开始， 第n回合
+            StartCoroutine(OneStart());
         }
         else
         {
