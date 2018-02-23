c794ebd956b400f887c9f88b95ac1e24d4e882dc
Xnco <xuanyylhky@163.com>
2018-02-23 18:02:21 +0800

    Update Logic and Atlas
    更新图集和逻辑

--- Assets/Resources/Scripts/PlayerEvent/PlayerEvent.cs
+++ Assets/Resources/Scripts/PlayerEvent/PlayerEvent.cs
@@ -6,4 +6,6 @@ public enum PlayerEvent
     BeginReply,  // Main 通知 玩家 和 AI 开始答题
 
     AI_Reply, // AI 回答问题
+
+    GameOver, // 游戏结束
 }
--- Assets/Resources/UI/Main/Scripts/UIWinner.cs
+++ Assets/Resources/UI/Main/Scripts/UIWinner.cs
@@ -2,21 +2,56 @@
 using UnityEngine.SceneManagement;
 using System.Collections;
 
-public class UIWinner : MonoBehaviour {
+public class UIWinner : MonoBehaviour
+{
+    Transform mCrown;
+    EventManager manager;
+
+    bool iswinner;
 
     void Awake()
     {
+        mCrown = transform.Find("Crown");
+        manager = EventManager.GetSinglon();
+        manager.RegisterMsgHandler((int)PlayerEvent.GameOver, OnGameOver);
+
         Transform back = transform.Find("Body/Again");
         if (back != null)
         {
             UIEventListener.Get(back.gameObject).onClick +=
                 (go) =>
                 {
-                    SceneManager.LoadScene("RankPK");
-
                     // 胜利 增加星星
-                    Player.GetSingle().pExp++;
+                    if (iswinner)
+                    {
+                        Player.GetSingle().pExp++;
+                    }
+                    else
+                    {
+                        // 失败 隐藏皇冠
+                        UIHelper.SetActive(mCrown, false);
+                    }
+
+                    SceneManager.LoadScene("RankPK");
                 };
         }
+    }
+
+    void OnGameOver(BaseEvent varData)
+    {
+        if (varData == null)
+        {
+            return;
+        }
+
+        ExData<bool> tmpdata = varData as ExData<bool>;
+        iswinner = tmpdata.data;
+
+        GetComponent<TweenScale>().PlayForward();
+    }
+
+    void OnDestroy()
+    {
+        manager.UnRegisterMsgHandler((int)PlayerEvent.GameOver, OnGameOver);
     }
 }
